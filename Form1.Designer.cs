cf9964c1ef4a2b4a3b77a374dc70ffb3b4d10984
EvanWhiteRoy <Evanwhit225@ed.amdsb.ca>
2024-06-22 14:04:12 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace FlappyBirdV2
+{
+    partial class flappyBird
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.subLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.scorelabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick_1);
+            // 
+            // subLabel
+            // 
+            this.subLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subLabel.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subLabel.ForeColor = System.Drawing.Color.Orange;
+            this.subLabel.Location = new System.Drawing.Point(222, 159);
+            this.subLabel.Name = "subLabel";
+            this.subLabel.Size = new System.Drawing.Size(333, 43);
+            this.subLabel.TabIndex = 0;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.Orange;
+            this.titleLabel.Location = new System.Drawing.Point(286, 112);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(189, 23);
+            this.titleLabel.TabIndex = 1;
+            // 
+            // scorelabel
+            // 
+            this.scorelabel.BackColor = System.Drawing.Color.Transparent;
+            this.scorelabel.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scorelabel.ForeColor = System.Drawing.Color.Orange;
+            this.scorelabel.Location = new System.Drawing.Point(308, 20);
+            this.scorelabel.Name = "scorelabel";
+            this.scorelabel.Size = new System.Drawing.Size(100, 23);
+            this.scorelabel.TabIndex = 2;
+            // 
+            // flappyBird
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FlappyBirdV2.Properties.Resources.OIP__1_;
+            this.ClientSize = new System.Drawing.Size(734, 261);
+            this.Controls.Add(this.scorelabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.subLabel);
+            this.DoubleBuffered = true;
+            this.Name = "flappyBird";
+            this.Text = "Flappy Bird";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.flappyBird_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.flappyBird_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.flappyBird_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label subLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label scorelabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,247 @@
+﻿using FlappyBirdV2.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace FlappyBirdV2
+{
+    public partial class flappyBird : Form
+    {
+        //Player rectangle
+        Rectangle player1 = new Rectangle(80, 90, 20, 20);
+        
+
+        SolidBrush greenBrush = new SolidBrush(Color.Black);
+
+        //Variables
+        int gravity = 23;
+        int playerScore = 0;
+        int pipeWidth = 40;
+        int pipeHeight = 120;
+        int pipeGap = 120; // Gap between top and bottom pipes
+        int minPipeDistance = 200; // Minimum dstance between pipes
+        int time = 0;
+
+
+        Image birdImg = Properties.Resources.Bird;
+        Image topPipeImg = Properties.Resources.topPipe;
+        Image bottomPipeImg = Properties.Resources.bottomPipe;
+
+
+        //Sounds for interactions
+
+        SoundPlayer jump = new SoundPlayer(Properties.Resources.Jump);
+        SoundPlayer lose = new SoundPlayer(Properties.Resources.Lose);
+
+
+
+
+
+        //Lists of pipes
+        List<Rectangle> topPipes = new List<Rectangle>();
+        List<Rectangle> bottomPipes = new List<Rectangle>();
+
+        //Player keybinds
+        bool spacePressed = false;
+
+        //Random generator
+        Random randGen = new Random();
+        
+
+
+
+        public flappyBird()
+        {
+            InitializeComponent();
+            GameIntialize();
+        }
+
+
+        //paint the game
+        private void flappyBird_Paint(object sender, PaintEventArgs e)
+        {
+            if (gameTimer.Enabled == false && time == 0 )
+            {
+                //Starting screen
+                titleLabel.Text = "Flappy Bird";
+                subLabel.Text = "Press Space to Start or Esc to Exit";
+
+            }
+            else if (gameTimer.Enabled == true)
+            {
+                //Update score display
+                scorelabel.Text = $"{time}";
+                e.Graphics.DrawImage(birdImg, player1);
+
+
+
+
+                for (int i = 0; i < topPipes.Count; i++)
+                {
+                    e.Graphics.DrawImage(topPipeImg, topPipes[i]);
+                }
+
+                for (int i = 0; i < bottomPipes.Count; i++)
+                {
+                    e.Graphics.DrawImage(bottomPipeImg, bottomPipes[i]);
+                }
+            }
+            else if (gameTimer.Enabled == false && time != 0)
+            {
+                titleLabel.Text = "Game Over";
+                subLabel.Text = "Press space to play again or esc to exit";
+            }
+
+        }
+
+        //Spawn the pipe so that there are spaces between the bottom pipes and the top pipes.
+        private void SpawnPipes()
+        {
+            if (topPipes.Count == 0 || topPipes.Last().X < 470)
+            {
+                int randY = randGen.Next(-50, 0); // Random height for top pipe
+                Rectangle topPipe1 = new Rectangle(600, randY, pipeWidth, pipeHeight);
+                topPipes.Add(topPipe1);
+
+                // Bottom pipe with a gap from the top pipe
+                Rectangle bottomPipe1 = new Rectangle(600, randY + pipeHeight + pipeGap - 50 , pipeWidth, pipeHeight);
+                bottomPipes.Add(bottomPipe1);
+            }
+
+            
+        }
+
+        
+
+        private void PlayerIntersection()
+        {
+            for (int i = 0; i < topPipes.Count; i++)
+            {
+                if (player1.IntersectsWith(topPipes[i]) || player1.IntersectsWith(bottomPipes[i]))
+                {
+                    gameTimer.Stop();
+                    lose.Play();
+
+                }
+
+            }
+        }
+
+
+
+        //bird movement
+        private void flappyBird_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            switch (e.KeyCode)
+            {
+                case Keys.Space:
+                    spacePressed = true;
+                    
+
+                    if (gameTimer.Enabled == false && time== 0)
+                    {
+                        GameIntialize();
+                    }
+                    if (gameTimer.Enabled == false && time != 0)
+                    {
+                        GameIntialize();
+                    }
+
+                    break;
+
+                //escape button for exiting before the game starts and space button for starting the game
+                case Keys.Escape:
+                    if (gameTimer.Enabled == false)
+                    {
+                        Application.Exit();
+                    }
+                    break;
+
+            }
+                    
+        }
+
+        private void flappyBird_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.Space:
+                    spacePressed = false;
+                    break;
+            }
+        }
+
+
+        //initialize game when timer is true
+        public void GameIntialize()
+        {
+            //Reset
+            topPipes.Clear();
+            bottomPipes.Clear();
+            time = 0;
+            titleLabel.Text = "";
+            subLabel.Text = "";
+
+            gameTimer.Enabled = true;
+
+            playerScore = 0;
+           
+           
+            topPipes.Clear();
+            bottomPipes.Clear();
+            
+
+            player1 = new Rectangle(80, 90, 20, 20);
+           
+
+        }
+
+        //the main game mechanic
+        private void gameTimer_Tick_1(object sender, EventArgs e)
+        {
+            time++;
+            PlayerIntersection();
+            SpawnPipes();
+            if (player1.Y < this.Height - player1.Height)
+            {
+                player1.Y += 3;
+            }
+
+            for (int i = 0; i < topPipes.Count; i++)
+            {
+                topPipes[i] = new Rectangle(topPipes[i].X - 2, topPipes[i].Y, topPipes[i].Width, topPipes[i].Height);
+            }
+
+            for (int i = 0; i < bottomPipes.Count; i++)
+            {
+                bottomPipes[i] = new Rectangle(bottomPipes[i].X - 2, bottomPipes[i].Y, bottomPipes[i].Width, bottomPipes[i].Height);
+            }
+
+            // Remove pipes that have moved off screen
+            if (topPipes.Count > 0 && topPipes[0].X < - pipeWidth)
+            {
+                topPipes.RemoveAt(0);
+                bottomPipes.RemoveAt(0);
+            }
+
+
+            if (spacePressed == true)
+            {
+                jump.Play();
+                player1.Y -= gravity;
+            }
+            Refresh();
+        
+        }
+    }
+}
